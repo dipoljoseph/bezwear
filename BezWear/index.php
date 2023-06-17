5c2894d2107ce00997150ac96c172fedaae85368
dipoljoseph <97227464+dipoljoseph@users.noreply.github.com>
2023-06-17 17:52:45 +0800

    Add files via upload

---
+++ BezWear/index.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" type="image/jpg" href="images\tabicon.png"/>
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+    <title>BezWear</title>
+</head>
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo">BezWear</label>
+        <ul> 
+            <li><a href="#" class="active">New & Featured</a></li>
+            <li><a href="#">Men</a></li>
+            <li><a href="#">Women</a></li>
+            <li><a href="#">Kids</a></li>
+            <li><a href="#">Sale</a></li>
+        </ul>
+    </nav>
+
+    <div class="banner">
+        <img src="images\banner1.jpg" class="banner-img">
+
+        <p>Lorem, ipsum dolor</p>
+        <h1>NEW T-SHIRTS</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+        <button>Shop</button>
+        <button>Download the BezWear App</button>
+    </div>
+
+    <div class="featured">
+        <h3>Featured</h3>
+        <div class="column">
+            <div class="l-col container">
+                <img src="images\tshirt1.jpg">
+                <div class="img-overlay-text">
+                    <p>T-Shirt 1</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet</p>
+                    <button>Get It First</button>
+                </div>
+            </div>
+            <div class="r-col container">
+                <img src="images\tshirt2.jpg">
+                <div class="img-overlay-text">
+                    <p>T-Shirt 2</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet</p>
+                    <button>Shop</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="quiz">
+        <div class="backdrop">
+            <div class="frontdrop">
+                <p>What's Your Style?</p>
+                <h1>FIND GEAR THAT SUITS YOU</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                <button>Start Quiz</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="latest">
+        <h3>The Latest</h3>
+        <img src="images\banner2.jpg" class="latest-img">
+
+        <p>Lorem, ipsum dolor</p>
+        <h1>LATEST SHIRTS</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+        <button>Shop</button>
+    </div>
+
+    <div class="hor-shop">
+        <h3>Modest Wear</h3>
+        <div class="scrollmenu">
+            <div class="hor-item">
+                <img src="images\tshirt1.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="hor-item">
+                <img src="images\tshirt1.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="hor-item">
+                <img src="images\tshirt1.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="hor-item">
+                <img src="images\tshirt1.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="hor-item">
+                <img src="images\tshirt1.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="featured">
+        <h3>Trending</h3>
+        <div class="column">
+            <div class="l-col container">
+                <img src="images\tshirt3.jpg">
+                <div class="img-overlay-text">
+                    <p>T-Shirt 1</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet</p>
+                    <button>Get It First</button>
+                </div>
+            </div>
+            <div class="r-col container">
+                <img src="images\tshirt4.jpg">
+                <div class="img-overlay-text">
+                    <p>T-Shirt 2</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet</p>
+                    <button>Shop</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="hor-shop">
+        <h3>Athletic Dept.</h3>
+        <div class="scrollmenu">
+            <div class="hor-item">
+                <img src="images\tshirt2.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="hor-item">
+                <img src="images\tshirt2.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="hor-item">
+                <img src="images\tshirt2.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="hor-item">
+                <img src="images\tshirt2.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="hor-item">
+                <img src="images\tshirt2.jpg">
+                <b>
+                    <p>Item Name
+                        <span>₱1,000</span>
+                    </p>
+                </b>
+                <p class="hor-itemdesc">Lorem ipsum dolor sit amet</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="just-in">
+        <h3>Just In</h3>
+        <video width="100%" height="100%" autoplay muted>
+            <source src="images/video demo.mp4" type="video/mp4" />
+        </video>
+
+        <p>Lorem, ipsum dolor</p>
+        <h1>NEWLY SEWN ITEMS</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+        <button>Shop</button>
+    </div>
+
+    <div class="essentials">
+        <h3>The Essentials</h3>
+        <div class="column">
+            <div class="left">
+                <div class="container">
+                    <img src="images\mens.jpg">
+                    <div class="img-overlay-text">
+                        <button>Men's</button>
+                    </div>
+                </div>
+            </div>
+            <div class="mid">
+                <div class="container">
+                    <img src="images\womens.jpg">
+                    <div class="img-overlay-text">
+                        <button>Women's</button>
+                    </div>
+                </div>
+            </div>
+            <div class="right">
+                <div class="container">
+                    <img src="images\kids.jpg">
+                    <div class="img-overlay-text">
+                        <button>Kid's</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="items">
+        <div class="column">
+            <div class="oneforth">
+                <p>Icons</p>
+                <ul>
+                    <li><a href="">Item 1</a></li>
+                    <li><a href="">Item 2</a></li>
+                    <li><a href="">Item 3</a></li>
+                    <li><a href="">Item 4</a></li>
+                    <li><a href="">Item 5</a></li>
+                </ul>
+            </div>
+            <div class="oneforth">
+                <p>Bedsheets</p>
+                <ul>
+                    <li><a href="">Item 1</a></li>
+                    <li><a href="">Item 2</a></li>
+                    <li><a href="">Item 3</a></li>
+                    <li><a href="">Item 4</a></li>
+                    <li><a href="">Item 5</a></li>
+                </ul>
+            </div>
+            <div class="oneforth">
+                <p>Clothing</p>
+                <ul>
+                    <li><a href="">Item 1</a></li>
+                    <li><a href="">Item 2</a></li>
+                    <li><a href="">Item 3</a></li>
+                    <li><a href="">Item 4</a></li>
+                    <li><a href="">Item 5</a></li>
+                </ul>
+            </div>
+            <div class="oneforth">
+                <p>Kids'</p>
+                <ul>
+                    <li><a href="">Item 1</a></li>
+                    <li><a href="">Item 2</a></li>
+                    <li><a href="">Item 3</a></li>
+                    <li><a href="">Item 4</a></li>
+                    <li><a href="">Item 5</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="column">
+            <div class="oneforth">
+                <ul>
+                    <li><a href="">FIND A STORE</a></li>
+                    <li><a href="">BECOME A MEMBER</a></li>
+                    <li><a href="">SIGNUP FOR EMAIL</a></li>
+                    <li><a href="">STUDENT DISCOUNTS</a></li>
+                    <li><a href="">SEND US FEEDBACK</a></li>
+                </ul>
+            </div>
+
+            <div class="oneforth">
+                <ul>
+                    <li><a href="">GET HELP</a></li>
+                    <li><a href="" class="f-gray">Order Status</a></li>
+                    <li><a href="" class="f-gray">Delivery</a></li>
+                    <li><a href="" class="f-gray">Returns</a></li>
+                    <li><a href="" class="f-gray">Payment Options</a></li>
+                    <li><a href="" class="f-gray"class="f-gray">Contact Us</a></li>
+                </ul>
+            </div>
+            
+            <div class="oneforth">
+                <ul>
+                    <li><a href="">ABOUT BEZWEAR</a></li>
+                    <li><a href="" class="f-gray">News</a></li>
+                    <li><a href="" class="f-gray">Careers</a></li>
+                    <li><a href="" class="f-gray">Investors</a></li>
+                    <li><a href="" class="f-gray">Sustainability</a></li>
+                </ul>
+            </div>
+
+            <div class="oneforth f-right">
+                <ul>
+                    <img src="images\fb1.png" class="f-soc invert">
+                    <img src="images\msgr1.png" class="f-soc invert">
+                    <img src="images\ig1.png" class="f-soc invert">
+                    <img src="images\whatsapp1.png" class="f-soc invert">
+                </ul>
+            </div>
+        </div>
+
+        <div class="copyright">
+            <img src="images\location1.png" class="f-soc invert">
+            <span>Angono Rizal</span>
+            <span class="cp-gray">© 2023 BezWear, Inc. All Rights Reserved</span>
+
+            <span class="cp-right">
+                <a href="">Privacy Policy</a>
+                <a href="">Terms of Use</a>
+                <a href="">Terms of Sale</a>
+                <a href="">Guides</a>
+            </span>
+        </div>
+    </div>
+</body>
+</html>
